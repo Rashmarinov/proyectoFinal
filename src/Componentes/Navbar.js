4f595b5885f67ecf9228a605b47b84e96d331437
Rashmarinov <98330380+Rashmarinov@users.noreply.github.com>
2023-02-21 17:13:01 +0100

    Creación de carpetas

---
+++ src/Componentes/Navbar.js
@@ -0,0 +1,24 @@
+const Navbar = () => (
+  <div class="navlist">
+    <ul>
+      <li>
+        <a href={"/"}>Inicio</a>
+      </li>
+      <li>
+        <a href={"/mis-partidas"}>Mis partidas</a>
+      </li>
+      <li>
+        <a href={"/añadir-partida"}>Añadir partida</a>
+      </li>
+
+      <li>
+        <a href={"/sign-in"}>Sign in</a>
+      </li>
+      <li>
+        <a href={"/registrate"}>Registrate</a>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Navbar;
