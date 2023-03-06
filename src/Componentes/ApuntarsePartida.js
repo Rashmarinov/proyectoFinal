a4d1247b35486d48fcb7ce9bbcccefd842ecc86d
Rashmarinov <98330380+Rashmarinov@users.noreply.github.com>
2023-03-06 11:19:58 +0100

    Componente 'ApuntarsePartida' corregido, ahora carga correctamente los datos del .json a través del id de la partida

--- src/Componentes/ApuntarsePartida.js
+++ src/Componentes/ApuntarsePartida.js
@@ -1,43 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import { useParams } from "react-router-dom";
-// import Navbar from "./Navbar";
-
-// const ApuntarsePartida = (props) =>{
-
-//     const [partida, setPartida] = useState();
-//     const { id } = useParams();
-
-//     const fetchApi = () => {
-//         fetch("data.json" + id)
-//             .then((response) => response.json())
-//             .then((data) => {
-//                 setPartida(data);
-//             });
-//     };
-
-//     useEffect(() => {
-//         fetchApi();
-//     }, [props]);
-
-//         return (
-//     <div className="containerApuntarse">   
-//     <section className="apuntarsePartida" >
-//       <p className="apuntarsePartida--Info">Juego: {partida.juego[id]}</p>
-//       <p className="apuntarsePartida--Info">Jugadores: {partida.jugadores}</p>
-//       <p className="apuntarsePartida--Info">Fecha: {partida.fecha}</p>
-//       <p className="apuntarsePartida--Info">Hora: {partida.hora}</p>
-//       <p className="apuntarsePartida--Info">Ubicación: {partida.ubicacion}</p>
-//       <button className="SignIn--button">Apuntarse</button>
-//     </section>
-    
-//     <Navbar></Navbar>
-//   </div>
-// );
-      
-// }
-// export default ApuntarsePartida;
-
-
 
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
@@ -47,30 +7,30 @@ function ApuntarsePartida() {
   const [partida, setPartida] = useState({});
   const { id } = useParams();
 
-  useEffect(() => {
-    fetch("data.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const partidaEncontrada = data.find((juego) => juego.id === id);
-        setPartida(partidaEncontrada);
-      })
-      .catch((error) => console.log(error));
-  }, [id]);
+useEffect(() => {
+  fetch(process.env.PUBLIC_URL + "/data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const partidaEncontrada = data.find((juego) => juego.id === parseInt(id));
+      setPartida(partidaEncontrada);
+    })
+    .catch((error) => console.log(error));
+}, [id]);
 
   return (
     <div className="containerApuntarse">   
-      <section className="apuntarsePartida" key={partida.id}>
+      <section className="apuntarsePartida" >
         <p className="apuntarsePartida--Info">Juego: {partida.juego}</p>
-        <p className="apuntarsePartida--Info">Jugadores: {partida.jugadores}</p>
+        <p className="apuntarsePartida--Info">Jugadores: {partida.jugadores.join(", ")}</p>
         <p className="apuntarsePartida--Info">Fecha: {partida.fecha}</p>
         <p className="apuntarsePartida--Info">Hora: {partida.hora}</p>
         <p className="apuntarsePartida--Info">Ubicación: {partida.ubicacion}</p>
         <button className="SignIn--button">Apuntarse</button>
       </section>
-      
       <Navbar></Navbar>
     </div>
   );
 }
 
 export default ApuntarsePartida;
+
